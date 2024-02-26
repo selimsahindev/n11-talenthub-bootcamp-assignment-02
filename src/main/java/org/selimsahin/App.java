c43b82aa4c591559eb2952951f0e4961f065270f
Selim Şahin <selimsahin.sns@gmail.com>
2024-02-26 19:18:29 +0300

    Initial commit.

---
+++ src/main/java/org/selimsahin/App.java
@@ -0,0 +1,13 @@
+package org.selimsahin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
